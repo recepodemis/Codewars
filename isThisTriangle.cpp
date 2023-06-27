eb2cd5e77d497c8592e79742d0765429eb6138db
Recep ODEMİS <recepodemis6@gmail.com>
2023-06-27 12:27:10 +0300

    is this triangle function added

---
+++ isThisTriangle.cpp
@@ -0,0 +1,19 @@
+/*
+
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+*/
+
+namespace Triangle
+{
+  bool isTriangle(unsigned int a, unsigned int b,unsigned int c)
+  {
+    if(a+b>c && a+c>b && c+b>a)
+    return true;
+    else
+      return false;
+  }
+};
+
